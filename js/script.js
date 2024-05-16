ab3954fb1794a67dc759434388e509128392a038
Lucky Perez <luckyperez321@gmail.com>
2024-05-16 09:41:37 -0500

    javascript is checked. Need to proof read HTML

--- js/script.js
+++ js/script.js
@@ -3,29 +3,29 @@ const username = "luckyperez321"; //console.log(luckyperez321);
 
 const githubInfo = async function () {
     const url = await fetch(`https://api.github.com/users/${username}`);
-    console.log(url);
     const data = await url.json();
-    console.log(data);
+    displayInfo(data);
 };
-githubInfo(data);
+githubInfo();
 
 
-const displayinfo = function (data) {
+
+const displayInfo = function (data) {
     const userInfoDiv = document.createElement('div');
     userInfoDiv.classList.add('user-info');
     userInfoDiv.innerHTML = `
 
 
 <figure>
-    <img alt = "user avatar" src=${}/>
+    <img alt = "user avatar" src="${data.avatar_url}"/>
 </figure>
 <div>
-<p><strong>Name:</strong>${}</p>
-<p><strong>Bio:</strong>${}</p>
-<p><strong>Location:</strong>${}</p>
-<p><strong>Number of public repos:</strong>${}</p>
+<p><strong>Name:</strong>${data.name}</p>
+<p><strong>Bio:</strong>${data.bio}</p>
+<p><strong>Location:</strong>${data.location}</p>
+<p><strong>Number of public repos:</strong>${data.public_repos}</p>
 </div>
-`
+`;
     overview.appendChild(userInfoDiv);
 
 };
